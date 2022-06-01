baa5a3e3169dde80ebe6eedcfd431a0fcb477537
Gabriel Lobo <gabrielrblgbi@gmail.com>
2022-06-01 02:49:42 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: global.__MONGO_URI__ || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT ?? 5050
+  port: process.env.PORT ?? 5050,
+  jwtSecret: process.env.JWT_SECRET ?? 'guBB==2Lksm'
 }
