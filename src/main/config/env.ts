3838ba2f5c88f8147ab02ddd6f28bc0c6d251be9
Gabriel Lobo <gabrielrblgbi@gmail.com>
2022-06-03 09:49:23 -0300

    fix: change MONGO_URL var

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: global.__MONGO_URI__ || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT ?? 5050,
   jwtSecret: process.env.JWT_SECRET ?? 'guBB==2Lksm'
 }
