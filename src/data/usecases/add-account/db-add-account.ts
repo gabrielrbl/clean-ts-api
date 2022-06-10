064d99b16206c880899d38b38c9626e443c033f9
Gabriel Lobo <gabrielrblgbi@gmail.com>
2022-06-09 21:54:38 -0300

    fix: add AccountMongoRepository on DbAddAccount composition

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -11,7 +11,7 @@ export class DbAddAccount implements AddAccount {
   constructor (
     private readonly hasher: Hasher,
     private readonly addAccountRepository: AddAccountRepository,
-    private readonly loadAccountByEmailRepository?: LoadAccountByEmailRepository
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   ) {}
 
   async add (accountData: AddAccountModel): Promise<AccountModel | null> {
