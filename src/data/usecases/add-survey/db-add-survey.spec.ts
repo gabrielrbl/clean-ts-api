651cbce967e065c894182fb5640bfb7bfe0c8720
Gabriel Lobo <gabrielrblgbi@gmail.com>
2022-06-20 22:11:07 -0300

    feat: ensure DbSaveSurveyResult calls SaveSurveyResultRepository with correct values

--- src/data/usecases/add-survey/db-add-survey.spec.ts
+++ src/data/usecases/add-survey/db-add-survey.spec.ts
@@ -1,5 +1,5 @@
-import { AddSurveyModel, AddSurveyRepository } from './db-add-survey-protocols'
 import { DbAddSurvey } from './db-add-survey'
+import { AddSurveyModel, AddSurveyRepository } from './db-add-survey-protocols'
 import MockDate from 'mockdate'
 
 const makeFakeSurveyData = (): AddSurveyModel => ({
