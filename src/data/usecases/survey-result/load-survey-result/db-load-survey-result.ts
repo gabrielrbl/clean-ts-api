6dc2f63c7dc6dfb31530c04c889dc52c411158cf
Gabriel Lobo <gabrielrblgbi@gmail.com>
2022-07-18 02:12:20 -0300

    feat: ensure DbLoadSurveyResult calls LoadSurveyResultRepository with correct values

---
+++ src/data/usecases/survey-result/load-survey-result/db-load-survey-result.ts
@@ -0,0 +1,14 @@
+import { SurveyResultModel } from '@/domain/models/survey-result'
+import { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
+import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
+
+export class DbLoadSurveyResult implements LoadSurveyResult {
+  constructor (
+    private readonly loadSurveyResultRepository: LoadSurveyResultRepository
+  ) {}
+
+  async load (surveyId: string): Promise<SurveyResultModel> {
+    await this.loadSurveyResultRepository.loadBySurveyId(surveyId)
+    return null
+  }
+}
